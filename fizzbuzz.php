02cfed0e2246a575fa0b12a2425d6c4b4ec0d684
Shuto-Toyama <touyamashuuto@gmail.com>
2021-06-07 16:27:16 +0900

    fizzbuzz

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,8 +1,35 @@
 <?php
-
-
 for ($i=1; $i < 101 ; $i++) {
-  echo $i."<br>";
+  if($i % 3 == 0 && $i % 5 == 0) {
+    echo "FizzBuzz<br>";
+    continue;
+  }elseif($i % 3 == 0){
+    echo "Fizz<br>";
+    continue;
+  }elseif($i % 5 == 0){
+    echo "Buzz<br>";
+    continue;
+  }else{
+    echo $i."<br>";
+  }
+}
+?>
+
+<?php 
+
+for($i=1; $i < 101; $i++) {
+ if($i % 3 == 0 && $i % 5 == 0) {
+   echo "FizzBuzz<br>";
+   continue;
+ }elseif($i % 3 == 0) {
+   echo "Fizz<br>";
+   continue;
+ }elseif($i % 5 == 0) {
+   echo "Buzz<br>";
+   continue;
+ }else{
+   echo $i."<br>";
+ }
 }
 
 ?>
